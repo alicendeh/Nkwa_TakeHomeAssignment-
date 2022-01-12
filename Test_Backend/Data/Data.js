b031285cbc695196a4e9db65a7bc36128d7e8222
alicendeh <alicendeh16@gmail.com>
2022-01-12 16:47:14 +0100

    Added the JSON Data to a seperate file

---
+++ Test_Backend/Data/Data.js
@@ -0,0 +1,77 @@
+const userData = [
+  {
+    id: 1,
+    name: "Alice Ndeh",
+    individualPoints: [
+      {
+        title: "Finished goal",
+        points: 220,
+        date: "01/12/2020",
+      },
+      {
+        title: "Saved today",
+        points: 540,
+        date: "11/12/2021",
+      },
+      {
+        title: "Saved 15000",
+        points: 990,
+        date: "02/02/2020",
+      },
+      {
+        title: "Goal attain",
+        points: 4,
+        date: "01/02/2021",
+      },
+      {
+        title: "Goal attained",
+        points: 2,
+        date: "02/03/2020",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Yaya Mamoudou",
+    individualPoints: [
+      {
+        title: "Finished goal",
+        points: 0,
+        date: "02/03/2020",
+      },
+      {
+        title: "Saved today",
+        points: 990,
+        date: "02/03/2020",
+      },
+      {
+        title: "Saved 15000",
+        points: 0,
+        date: "02/07/2021",
+      },
+      {
+        title: "Goal attain",
+        points: 2,
+        date: "01/03/2020",
+      },
+      {
+        title: "Goal attain",
+        points: 2,
+        date: "02/01/2020",
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Njume Yannick",
+    individualPoints: [
+      {
+        title: "Finished goal",
+        points: 20,
+        date: "02/07/2021",
+      },
+    ],
+  },
+];
+
+module.exports = userData;
